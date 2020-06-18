0080b9270bbf244bd88c387e5b2e27d1e869b3c0
Alison Maziero <alison_lens@hotmail.com>
2020-06-17 21:23:21 -0300

    fixed bug in the link api

--- Next Level Week - 1/Starter/scripts/create-point.js
+++ Next Level Week - 1/Starter/scripts/create-point.js
@@ -1,21 +1,13 @@
-// document
-//   .querySelector("select[name=uf]")
-//   .addEventListener("chage", () => {
-
-//   });
-
 function populateUFs() {
   const ufSetect = document.querySelector("select[name=uf]");
 
-  fetch("https://servicodados.ibge.gov.br/api/v1/localidades/distritos")
+  fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
     .then((response) => {
       return response.json();
     })
     .then((states) => {
-      for (state of states) {
-        ufSetect.innerHTML += `
-          <option value="${state.id}" >${state.nome}</option>
-        `;
+      for (const state of states) {
+        ufSetect.innerHTML += `<option value="${state.id}">${state.nome}</option>`;
       }
     });
 }
