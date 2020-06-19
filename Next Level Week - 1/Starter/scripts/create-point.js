010ce743099dcb4ec7d4c787da20bc8ff4af5817
Alison Maziero <alison_lens@hotmail.com>
2020-06-18 21:38:54 -0300

    adding the city selection optin with javascript

--- Next Level Week - 1/Starter/scripts/create-point.js
+++ Next Level Week - 1/Starter/scripts/create-point.js
@@ -7,9 +7,34 @@ function populateUFs() {
     })
     .then((states) => {
       for (const state of states) {
-        ufSetect.innerHTML += `<option value="${state.id}">${state.nome}</option>`;
+        ufSetect.innerHTML += `
+          <option value="${state.id}">${state.nome}</option>
+        `;
       }
     });
 }
 
 populateUFs();
+
+function getCities(event) {
+  const citySelect = document.querySelector("select[name=city]");
+  const url = `
+      https://servicodados.ibge.gov.br/api/v1/localidades/estados/${event.target.value}/microrregioes
+    `;
+
+  fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((cities) => {
+      for (const city of cities) {
+        citySelect.innerHTML += `
+          <option value="${city.id}" >${city.nome}</option>
+        `;
+      }
+
+      citySelect.disabled = false;
+    });
+}
+
+document.querySelector("select[name=uf]").addEventListener("change", getCities);
