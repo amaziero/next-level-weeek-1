0566a5ec94ce8c89805e053d084bd5a9ed63d617
Alison Maziero <alison_lens@hotmail.com>
2020-07-16 19:57:54 -0300

    too many dependencies errors

--- Next Level Week - 1/Starter/frontend/server.js
+++ Next Level Week - 1/Starter/frontend/server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const server = express();
-const path = require("path");
 
-server.use(express.static("./public"));
+server.use(express.static(__dirname + "public"));
 
 server.get("/", (request, response) => {
   response.sendFile(__dirname + "/pages/home/index.html");
 });
+
 server.listen(3000);
