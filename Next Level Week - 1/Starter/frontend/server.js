c398a9274a155c1c8cae360300264a95d302f7a4
Alison Maziero <alison_lens@hotmail.com>
2020-07-16 21:09:23 -0300

    fixed bug

--- Next Level Week - 1/Starter/frontend/server.js
+++ Next Level Week - 1/Starter/frontend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const server = express();
 const path = require("path");
 
-server.use(express.static(path.join(__dirname, "/public")));
+server.use("/public", express.static("public"));
 
 server.get("/", (request, response) => {
   response.sendFile(__dirname + "/pages/index.html");
