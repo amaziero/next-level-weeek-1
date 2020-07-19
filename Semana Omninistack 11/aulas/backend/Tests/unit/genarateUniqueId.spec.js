bddfc0e7416143acb2a7e48f3c2602524d1aa810
Alison Maziero <alison_lens@hotmail.com>
2020-07-19 17:02:00 -0300

    changing repos

--- Semana Omninistack 11/aulas/backend/Tests/unit/genarateUniqueId.spec.js
+++
@@ -1,9 +0,0 @@
-const genarateUniqueId = require('../../src/utils/genarateUniqueId')
-
-describe('Genarate Unique Id for User', () => {
-  it('should genarate an unique ID for the user login', () => {
-    const id = genarateUniqueId();
-    
-    expect(id).toHaveLength(8);
-  })
-})
--- Semana Omninistack 11/aulas/backend/src/utils/genarateUniqueId.js
+++
@@ -1,6 +0,0 @@
-const crypto = require('crypto');
-
-module.exports = function genarateUniqueId() {
-  return crypto.randomBytes(4).toString('HEX');
-}
-
