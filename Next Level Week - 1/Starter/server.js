c4a63afe4a2e454ec3a921edb1af639d66f0dc91
Alison Maziero <alison_lens@hotmail.com>
2020-07-19 20:40:46 -0300

    post to register entities

--- Next Level Week - 1/Starter/server.js
+++ Next Level Week - 1/Starter/server.js
@@ -7,6 +7,8 @@ const db = require("./database/db");
 server.use("/public", express.static("public"));
 server.use("/scripts", express.static("scripts"));
 
+server.use(express.urlencoded({ extended: true }));
+
 nunjucks.configure("pages", {
   express: server,
   noCache: true,
@@ -17,10 +19,43 @@ server.get("/", (request, response) => {
 });
 
 server.get("/create-point", (request, response) => {
+  // request.query();
   return response.render("create-point.html");
 });
 
+server.post("/savepoint", (request, response) => {
+  const insertQuery = `INSERT INTO places ( name, image, adress, adress2, state, city, item )
+      VALUES (?, ?, ?, ?, ?, ?, ?);`;
+  const values = [
+    request.body.name,
+    request.body.image,
+    request.body.adress,
+    request.body.adress2,
+    request.body.state,
+    request.body.city,
+    request.body.item,
+  ];
+
+  function afterInsertData(err) {
+    if (err) {
+      console.error(err);
+      return response.send("Erro no cadastro!");
+    }
+    console.log("Salvo com Sucesso");
+
+    return response.render("create-point.html", { saved: true });
+  }
+
+  db.run(insertQuery, values, afterInsertData);
+});
+
 server.get("/search-results", (request, response) => {
+  const search = request.query.search;
+
+  if ((search = "")) {
+    return response.render("search-results.html", { total: 0 });
+  }
+
   db.all(`SELECT * FROM places`, function (error, rows) {
     if (error) {
       return console.error(error);
