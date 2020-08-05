e9637656d2d2448d3d4d859515629069d99fab20
Alison Maziero <alison_lens@hotmail.com>
2020-08-05 19:52:30 -0300

    routes exemplo

--- Next Level Week - 2/Boost/server/src/server.ts
+++ Next Level Week - 2/Boost/server/src/server.ts
@@ -2,8 +2,12 @@ import express from "express";
 
 const app = express();
 
-app.get("/users", (request, response) => {
-  response.send("hello word");
+app.post("/users", (request, response) => {
+  const users = [
+    { nome: "Alison", idade: 25 },
+    { nome: "Diego", idade: 25 },
+  ];
+  return response.send(users);
 });
 
 app.listen(3333);
