05f9a911bead09977c473d54b7e92f074889bc9b
Alison Maziero <alison_lens@hotmail.com>
2020-08-06 19:37:10 -0300

    creted database and the starter the controlers

---
+++ Next Level Week - 2/Boost/server/src/controllers/ClassesControllers.ts
@@ -0,0 +1,65 @@
+import { Request, Response } from "express";
+import db from "../database/connection";
+import convertHoursToMinutes from "../utils/convertHoursToMinutes";
+
+interface ScheduleItemProps {
+  week_day: number;
+  from: string;
+  to: string;
+}
+
+export default class ClassesController {
+  async create(request: Request, response: Response) {
+    const {
+      name,
+      avatar,
+      whatsapp,
+      bio,
+      subject,
+      cost,
+      schedule,
+    } = request.body;
+
+    const classTransaction = await db.transaction();
+
+    try {
+      const insertedUsersIds = await classTransaction("users").insert({
+        name,
+        avatar,
+        whatsapp,
+        bio,
+      });
+
+      const user_id = insertedUsersIds[0];
+
+      const insertedClassesId = await classTransaction("classes").insert({
+        subject,
+        cost,
+        user_id,
+      });
+
+      const class_id = insertedClassesId[0];
+
+      const classSchedule = schedule.map((schduleItem: ScheduleItemProps) => {
+        return {
+          class_id,
+          week_day: schduleItem.week_day,
+          from: convertHoursToMinutes(schduleItem.from),
+          to: convertHoursToMinutes(schduleItem.to),
+        };
+      });
+
+      await classTransaction("class_schedule").insert(classSchedule);
+
+      await classTransaction.commit();
+
+      return response.status(201).send();
+    } catch (err) {
+      await classTransaction.rollback();
+
+      return response.status(400).json({
+        error: "Unexpected error while creating new class",
+      });
+    }
+  }
+}
--- Next Level Week - 2/Boost/server/src/routes.ts
+++ Next Level Week - 2/Boost/server/src/routes.ts
@@ -1,60 +1,11 @@
 import express from "express";
-import db from "./database/connection";
-import convertHoursToMinutes from "./utils/convertHoursToMinutes";
+import ClassesController from "./controllers/ClassesControllers";
 
 const routes = express.Router();
 routes.use(express.json());
 
-interface ScheduleItemProps {
-  week_day: number;
-  from: string;
-  to: string;
-}
+const classesControler = new ClassesController();
 
-routes.post("/classes", async (request, response) => {
-  const { name, avatar, whatsapp, bio, subject, cost, schedule } = request.body;
-
-  const classTransaction = await db.transaction();
-
-  try {
-    const insertedUsersIds = await classTransaction("users").insert({
-      name,
-      avatar,
-      whatsapp,
-      bio,
-    });
-
-    const user_id = insertedUsersIds[0];
-
-    const insertedClassesId = await classTransaction("classes").insert({
-      subject,
-      cost,
-      user_id,
-    });
-
-    const class_id = insertedClassesId[0];
-
-    const classSchedule = schedule.map((schduleItem: ScheduleItemProps) => {
-      return {
-        class_id,
-        week_day: schduleItem.week_day,
-        from: convertHoursToMinutes(schduleItem.from),
-        to: convertHoursToMinutes(schduleItem.to),
-      };
-    });
-
-    await classTransaction("class_schedule").insert(classSchedule);
-
-    await classTransaction.commit();
-
-    return response.status(201).send();
-  } catch (err) {
-    await classTransaction.rollback();
-
-    return response.status(400).json({
-      error: "Unexpected error while creating new class",
-    });
-  }
-});
+routes.post("/classes", classesControler.create);
 
 export default routes;
