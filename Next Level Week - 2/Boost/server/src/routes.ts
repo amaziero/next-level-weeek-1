68ff54b6775edf343902e18ca5f2800c7e48f177
Alison Maziero <alison_lens@hotmail.com>
2020-08-06 20:43:57 -0300

    deleting data base

--- Next Level Week - 2/Boost/server/src/routes.ts
+++ Next Level Week - 2/Boost/server/src/routes.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import ClassesController from "./controllers/ClassesControllers";
+import ConnectionController from "./controllers/ConnectionsControlles";
 
 const routes = express.Router();
 routes.use(express.json());
 
 const classesControler = new ClassesController();
+const connectionControler = new ConnectionController();
 
 routes.post("/classes", classesControler.create);
 routes.get("/classes", classesControler.index);
+routes.get("/connections", connectionControler.create);
 
 export default routes;
