0cb60a4925bce7b9c55a165ea37c01e55ce29e40
Alison Maziero <alison_lens@hotmail.com>
2020-10-12 16:02:32 -0300

    day 1 trilha omninistack 3

---
+++ Next Level Week - 3/web/src/pages/OrphanagesMap.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import mapMarkerImg from '../images/map-marker.svg';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+import '../styles/pages/orphanages-map.css';
+import { Map, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+function OrphanegesMap() {
+  return (
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMarkerImg} alt="Map Marker" />
+
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estaram esperando a sua visita</p>
+        </header>
+
+        <footer>
+          <strong>Rio do Sul</strong>
+          <p>Santa catarina</p>
+        </footer>
+      </aside>
+
+      <Map
+        center={[-23.194049, -47.2666759]}
+        zoom={15}
+        style={{ width: '100%', height: '100%' }}
+      >
+        {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
+        <TileLayer
+          url={`https://api.mapbox.com/styles/v1/mapbox/navigation-preview-night-v4/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+        />
+      </Map>
+
+      <Link to="/" className="create-orphanage">
+        <FiPlus size={32} color="#FFF" />
+      </Link>
+    </div>
+  );
+}
+
+export default OrphanegesMap;
