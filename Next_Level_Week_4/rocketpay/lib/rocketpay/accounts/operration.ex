d6fc711a3d15fede1d65689b66db0c2a69fe638a
amaziero <alison_lens@hotmail.com>
2021-02-28 11:19:27 -0300

    refactor: abstracter withdraw and deposit logics

---
+++ Next_Level_Week_4/rocketpay/lib/rocketpay/accounts/operration.ex
@@ -0,0 +1,52 @@
+defmodule Rocketpay.Accounts.Operation do
+  alias Ecto.Multi
+  alias Rocketpay.{ Account }
+
+  def call( %{ "id" => id, "value" => value }, operation) do
+    Multi.new()
+    |> Multi.run(:account, fn repo, _changes -> get_account(repo, id) end)
+    |> Multi.run(:update_balance, fn repo, %{account: account} -> update_balance(repo, account, value, operation) end)
+  end
+
+  defp get_account(repo, id) do
+    case repo.get(Account, id) do
+      nil -> {:error, "Account not found, try again!"}
+      account -> {:ok, account}
+    end
+  end
+
+  defp update_balance(repo, account, value, operation) do
+    account
+    |> operations(value, operation)
+    |> udpate_account(repo, account)
+  end
+
+  defp operations(%Account{balance: balance}, value, operations) do
+    value
+    |> Decimal.cast()
+    |> handle_cast(balance, operations)
+  end
+
+  defp handle_cast({:ok, value}, balance, :deposit), do: Decimal.add(balance, value)
+
+  defp handle_cast({:ok, value}, balance, :withdraw), do: Decimal.sub(balance, value)
+
+  defp handle_cast(:error, _balance, _operation), do: {:error, "Invalid deposit value"}
+
+  defp udpate_account(value, repo, account) do
+    params = %{balance: value}
+
+    account
+    |> Account.changeset(params)
+    |> repo.update()
+  end
+
+  # defp udpate_account({:error, _reason} = error, _repo, _account), do: error
+
+  # defp run_transaction(multi) do
+  #   case Repo.transaction(multi) do
+  #     {:error, _operation, reason, _changes} -> {:error, reason}
+  #     {:ok, %{update_balance: account}} -> {:ok, account}
+  #   end
+  # end
+end
