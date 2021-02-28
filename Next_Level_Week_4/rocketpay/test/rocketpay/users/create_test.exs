d10e0b2305c5a9fb59a66ed3a166011b501165c9
amaziero <alison_lens@hotmail.com>
2021-02-28 19:49:57 -0300

    test:  tested create user

---
+++ Next_Level_Week_4/rocketpay/test/rocketpay/users/create_test.exs
@@ -0,0 +1,43 @@
+defmodule Rocketpay.Users.CreateTest do
+  use Rocketpay.DataCase
+
+  alias Rocketpay.User
+  alias Rocketpay.Repo
+  alias Rocketpay.Users.Create
+
+  describe "call/1" do
+    test "When all params are valid, return an user" do
+      params = %{
+        name: "alison",
+        password: "123123",
+        email: "alison@gmail.com",
+        nickname: "amaziero",
+        age: 25
+      }
+
+      {:ok, %User{id: user_id}} = Create.call(params)
+      user = Repo.get(User, user_id)
+
+      assert %User{
+        name: "alison",
+        age: 25,
+        id: ^user_id} = user
+    end
+
+    test "When there are invalid params, return an error" do
+      params = %{
+        name: "alison",
+        password: "123123",
+        email: "alison@gmail.com",
+        nickname: "amaziero",
+        age: 15
+      }
+
+      {:error, changeset} = Create.call(params)
+
+      expected_response = %{age: ["must be greater than or equal to 18"]}
+
+      assert errors_on(changeset) == expected_response
+    end
+  end
+end
